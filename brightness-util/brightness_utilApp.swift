d3d62f04071e0c11a2829f0269719c56c0d32815
Leo Cao <leo.c22@icloud.com>
2025-09-18 09:03:34 +1000

    Initial Commit

---
+++ brightness-util/brightness_utilApp.swift
@@ -0,0 +1,17 @@
+//
+//  brightness_utilApp.swift
+//  brightness-util
+//
+//  Created by Leo C on 18/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct brightness_utilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
